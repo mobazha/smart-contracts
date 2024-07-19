04920a39aaba49d664528b335eace980dc3d00ff
fengzie <526567244@qq.com>
2024-07-19 14:35:59 +0800

    Update

--- migrations/2_Escrow_migration.js
+++ migrations/2_Escrow_migration.js
@@ -2,15 +2,15 @@ var ScriptHashCalculator = artifacts.require("ScriptHashCalculator");
 var Escrow = artifacts.require("Escrow");
 
 async function doDeploy(deployer) {
-  // await deployer.deploy(ScriptHashCalculator);
-  // await deployer.link(ScriptHashCalculator, Escrow);
+  await deployer.deploy(ScriptHashCalculator);
+  await deployer.link(ScriptHashCalculator, Escrow);
 
-  const calculatorInstance = await ScriptHashCalculator.at('0x393b2FEfA82aB9ddFd7AF920C24A9dB0B27388c7');
-  await deployer.link(calculatorInstance, Escrow);
+  // const calculatorInstance = await ScriptHashCalculator.at('0x3DAe8BD5972D7D83A9661E13becd0C2dA9177F3B');
+  // await deployer.link(calculatorInstance, Escrow);
 
   // The second param is the MBZ token address
   // BSC: 0xBAD8470f50575Ac41d4FE1C31039554112d31E89
-  // Mumbai: 0x4c1A1b21c4471CA57145EE08404Cbaf9C8B83991
+  // Mumbai/Mainnet: 0x4c1A1b21c4471CA57145EE08404Cbaf9C8B83991
   // ConfluxTestnet/Mainnet: 0x4c1A1b21c4471CA57145EE08404Cbaf9C8B83991
   await deployer.deploy(Escrow, "0x4c1A1b21c4471CA57145EE08404Cbaf9C8B83991");
 }
